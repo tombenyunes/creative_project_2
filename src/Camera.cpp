0fdc237bd83fe9d542c89d9ca58ca9c6f2a9d4b1
Tom <tombenyunes@gmail.com>
2021-05-02 18:39:50 +0100

    clean up

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,6 +1,12 @@
 #include "Camera.h"
 
-Camera::Camera(): follow_player_(true)
+Camera::Camera()
+	:	follow_player_(true),
+		zooming_out_(false),
+		zooming_in_(false),
+		zooming_speed_(0.025f),
+		zoom_scale_upper_bound_(3.25f),
+		zoom_scale_lower_bound_(0.03f)
 {
 	cam_.disableMouseInput();
 	cam_.setVFlip(true);
@@ -21,15 +27,14 @@ Camera::Camera(): follow_player_(true)
 
 void Camera::update(const ofVec2f player_pos)
 {
-	if (follow_player_) {
-		cam_.setPosition(player_pos.x, player_pos.y, cam_.getPosition().z); // camera follows player
-	}
-	else {
-		cam_.setPosition(0 + HALF_WORLD_WIDTH, 0 + HALF_WORLD_HEIGHT, cam_.getPosition().z); // camera follows player
-	}
-	
-	// calculate local + world mouse positions
-	
+	calculate_mouse_coords();
+	follow_player(player_pos);
+	handle_zooming();
+}
+
+void Camera::calculate_mouse_coords()
+{
+	// calculate local + world mouse positions	
 	const ofVec3f local_pos = ofVec3f(ofGetMouseX() - HALF_WORLD_WIDTH, ofGetMouseY() - HALF_WORLD_HEIGHT, 0);
 	ofVec3f world_pos = screen_to_world(local_pos);
 	world_pos.x += WORLD_WIDTH * ofMap(get_scale().x, 1.0f, 3.0f, 0.0f, 1.0f);
@@ -37,6 +42,36 @@ void Camera::update(const ofVec2f player_pos)
 
 	local_mouse_pos_ = ofVec2f(ofGetMouseX() / 2 - ofGetWidth() / 2, ofGetMouseY() - ofGetHeight() / 2);
 	world_mouse_pos_ = world_pos;
+}
+
+void Camera::follow_player(const ofVec2f player_pos)
+{
+	// the camera follows the player
+	if (follow_player_) {
+		cam_.setPosition(player_pos.x, player_pos.y, cam_.getPosition().z); // camera follows player
+	}
+	else {
+		cam_.setPosition(0 + HALF_WORLD_WIDTH, 0 + HALF_WORLD_HEIGHT, cam_.getPosition().z); // camera follows player
+	}
+}
+
+void Camera::handle_zooming()
+{
+	// handle camera zooming in/out
+	if (zooming_in_)
+	{
+		if (cam_.getScale().x > zoom_scale_lower_bound_)
+		{
+			cam_.setScale(cam_.getScale().x - zooming_speed_, cam_.getScale().y - zooming_speed_, 1);
+		}
+	}
+	else if (zooming_out_)
+	{
+		if (cam_.getScale().x < zoom_scale_upper_bound_)
+		{
+			cam_.setScale(cam_.getScale().x + zooming_speed_, cam_.getScale().y + zooming_speed_, 1);
+		}		
+	}
 }
 
 ofVec3f Camera::get_local_mouse_pos() const
@@ -119,20 +154,32 @@ void Camera::draw() const
 }
 
 void Camera::key_pressed(const int key)
-{
-	if (key == 'z') {
-		toggle_zoom_mode();
+{	
+	if (key == 'q') {
+		zooming_out_ = true;
 	}
-	else if (key == 3682) {
+	else if (key == 'e') {
+		zooming_in_ = true;
+	}
+	else if (key == 3682) { // ctrl
 		ctrl_down_ = true;
+	}
+	else if (key == 9) { // tab
+		toggle_zoom_mode();
 	}
 }
 
 void Camera::key_released(const int key)
 {
-	if (key == 3682) {
-		ctrl_down_ = false;
+	if (key == 'q') {
+		zooming_out_ = false;
 	}
+	else if (key == 'e') {
+		zooming_in_ = false;
+	}
+	else if (key == 3682) { // ctrl
+		ctrl_down_ = false;
+	}	
 }
 
 void Camera::mouse_dragged(int x, int y, int button)
@@ -148,7 +195,7 @@ void Camera::mouse_pressed(int x, int y, int button)
 void Camera::mouse_scrolled(int x, int y, const float scroll_x, const float scroll_y)
 {
 	if (ctrl_down_) {
-		cam_.setScale(cam_.getScale().x + scroll_y / 10, cam_.getScale().y + scroll_y / 10, 1);
+		cam_.setScale(cam_.getScale().x - scroll_y / 10, cam_.getScale().y - scroll_y / 10, 1);
 		cout << "Zoom level: " << cam_.getScale() << endl;
 	}
 }
--- src/Camera.h
+++ src/Camera.h
@@ -8,27 +8,35 @@ class Camera {
 public:
 	
 	Camera();
-	
+
+	// Updated
 	void update(ofVec2f player_pos);
+	void calculate_mouse_coords();
+	void follow_player(ofVec2f player_pos);
+	void handle_zooming();		
+
+	// Helpers
+	void toggle_zoom_mode();
+
+	void begin();
+	void end();
+	
+	ofVec3f screen_to_world(ofVec3f view) const;
 
 	ofVec3f get_local_mouse_pos() const;
 	ofVec3f get_world_mouse_pos() const;
-	
-	void toggle_zoom_mode();
-
 	glm::mat4 get_local_transform_matrix() const;
 	glm::mat4 get_global_transform_matrix() const;
 	glm::mat4 get_projection_matrix() const;
 	glm::mat4 get_model_view_matrix() const;
 	glm::mat4 get_model_view_projection_matrix() const;
 	ofVec3f get_position() const;
-	glm::vec3 get_scale() const;
-	void begin();
-	void end();
-	ofVec3f screen_to_world(ofVec3f view) const;
+	glm::vec3 get_scale() const;	
 
+	// Draw Frustram
 	void draw() const;
-	
+
+	// Events
 	void key_pressed(int key);
 	void key_released(int key);
 	void mouse_dragged(int x, int y, int button);
@@ -42,5 +50,9 @@ private:
 	bool follow_player_;
 	ofVec3f local_mouse_pos_;
 	ofVec3f world_mouse_pos_;
-	
+	bool zooming_out_;
+	bool zooming_in_;
+	float zooming_speed_;
+	float zoom_scale_upper_bound_;
+	float zoom_scale_lower_bound_;
 };
